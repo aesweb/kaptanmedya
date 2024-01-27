2328ffea707acb086b39bf26f0b9248c8fc4b0ad
aesweb <aes.sncr@gmail.com>
2024-01-27 04:47:12 +0300

    feat: add Services component with images and text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import EventCard from '@/components/EventCard';
 import Footer from '@/components/Footer';
 import Hero from '@/components/Hero';
 import Navbar from '@/components/Navbar';
+import Services from '@/components/Services';
 
 export default function Home() {
   return (
