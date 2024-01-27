4aabf87c77c949549eb0593efae0696eeabc2fc3
aesweb <aes.sncr@gmail.com>
2024-01-27 03:15:23 +0300

    fix: remove unused svg files

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="gap-8 flex flex-col items-center justify-center">
+      <div className="flex items-center justify-center gap-12">
+        <div>instagram</div>
+        <div>twitter</div>
+        <div>youtube</div>
+        <div>123</div>
+        <div>123</div>
+      </div>
+      <div className="text-sm">Copyright Kaptan Medya Event Organization </div>
+    </div>
+  );
+};
+
+export default Footer;
+Footer;
