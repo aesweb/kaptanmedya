8f187a5c93fb214ee4a798c7a9ba8ea93cac3182
aesweb <aes.sncr@gmail.com>
2024-01-27 03:29:32 +0300

    style: add padding and update social icons in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,15 +2,22 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="gap-8 flex flex-col items-center justify-center">
+    <div className="gap-8 flex flex-col items-center justify-center py-24">
       <div className="flex items-center justify-center gap-12">
-        <div>instagram</div>
-        <div>twitter</div>
-        <div>youtube</div>
-        <div>123</div>
-        <div>123</div>
+        <div className="rounded-full bg-black text-white w-10 h-10 items-center justify-center flex">
+          x
+        </div>
+        <div className="rounded-full bg-black text-white w-10 h-10 items-center justify-center flex">
+          x
+        </div>
+        <div className="rounded-full bg-black text-white w-10 h-10 items-center justify-center flex">
+          x
+        </div>
+        <div className="rounded-full bg-black text-white w-10 h-10 items-center justify-center flex">
+          x
+        </div>
       </div>
-      <div className="text-sm">Copyright Kaptan Medya Event Organization </div>
+      <div className="text-sm">@Copyright Kaptan Medya Event Organization </div>
     </div>
   );
 };
