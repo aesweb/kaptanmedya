b1198c560cdfbebe24507951b088ab1fa7141d56
aesweb <aes.sncr@gmail.com>
2024-01-31 00:19:14 +0300

    feat: update Servisler section layout and styling

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -2,19 +2,10 @@ import React from 'react';
 import Image from 'next/image';
 const Services = () => {
   return (
-    <div>
-      <div className="flex flex-col gap-4 py-12 items-center justify-center text-center bg-white text-black">
-        <Image
-          src="/logo.jpeg"
-          alt="kaptanmedyalogosu"
-          height={70}
-          width={70}
-        />
-        <div>Servisler</div>
-        <div className="text-xl font-light pb-8">
-          “MUTLU ANLAR, EKSİKSİZ BİR GÜNLE BAŞLAR”
-        </div>
-        <div className="grid grid-cols-4 gap-8">
+    <div className="bg-white text-black py-24">
+      <div className="flex flex-col items-center justify-center text-center px-12">
+        <div className="text-3xl font-semibold py-12">Servislerimiz</div>
+        <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-16">
           <div className="items-center justify-center flex flex-col gap-4">
             <Image
               src="/logo.jpeg"
@@ -22,25 +13,12 @@ const Services = () => {
               height={120}
               width={120}
             />
-            <div className="text-lg">Servisler</div>
-            <div className="text-md px-8">
+            <div className="text-2xl">Servisler</div>
+            <div className="text-md font-light max-w-sm">
               Mutlu anlar, eksiksiz bir günle başlar. Evliliğe ilk adımınızı
               atarken yanınızdayız…
             </div>
-          </div>
-          <div className="items-center justify-center flex flex-col gap-4">
-            <Image
-              src="/logo.jpeg"
-              alt="kaptanmedyalogosu"
-              height={120}
-              width={120}
-            />
-            <div className="text-lg">Servisler</div>
-            <div className="text-md px-8">
-              Mutlu anlar, eksiksiz bir günle başlar. Evliliğe ilk adımınızı
-              atarken yanınızdayız…
-            </div>
-          </div>
+          </div>{' '}
           <div className="items-center justify-center flex flex-col gap-4">
             <Image
               src="/logo.jpeg"
