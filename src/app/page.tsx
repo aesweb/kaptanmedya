2d4948ab694f894008987ed06ea554cebce239f9
aesweb <aes.sncr@gmail.com>
2024-01-30 23:43:38 +0300

    feat: add new components and update SVG attributes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import { ImagesSliderDemo } from "@/components/ImagesSliderDemo";
+import EventCard from '@/components/EventCard';
+import { ImagesSliderDemo } from '@/components/ImagesSliderDemo';
+import { LayoutGridDemo } from '@/components/LayoutGridDemo';
+import OrganizationCard from '@/components/OrganizationCard';
+import Services from '@/components/Services';
 
 export default function Home() {
   return (
     <>
       <ImagesSliderDemo />
+      <EventCard />
+      <LayoutGridDemo />
+      <OrganizationCard />
+      <Services />
     </>
   );
 }
