eda8ff02d30e864a8b205e1502208f3d056edbaf
aesweb <aes.sncr@gmail.com>
2024-01-31 12:04:40 +0300

    feat: add parallax scroll and images slider to page

--- src/components/LayoutGridDemo.tsx
+++ src/components/LayoutGridDemo.tsx
@@ -4,7 +4,7 @@ import { LayoutGrid } from './ui/layout-grid';
 
 export function LayoutGridDemo() {
   return (
-    <div className="h-screen py-20 w-full">
+    <div className="h-screen py-12 w-full">
       <LayoutGrid cards={cards} />
     </div>
   );
