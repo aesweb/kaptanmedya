ec29fde8e0d94395772c3b2cb9a21aaed82e87c3
aesweb <aes.sncr@gmail.com>
2024-01-31 17:43:12 +0300

    refactor: optimize code for image slider in multiple pages

--- src/app/servisler/page.tsx
+++ src/app/servisler/page.tsx
@@ -1,11 +1,9 @@
 import { ImagesSliderDemo } from '@/components/ImagesSliderDemo';
-import { ParallaxScrollDemo } from '@/components/parallax-scroll-demo';
 
 export default function Page() {
   return (
     <section className="w-full">
       <ImagesSliderDemo />
-      <ParallaxScrollDemo />
     </section>
   );
 }
