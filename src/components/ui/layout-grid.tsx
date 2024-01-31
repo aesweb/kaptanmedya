51ea0d7d978c62c9062d49216f7babd525fd921a
aesweb <aes.sncr@gmail.com>
2024-01-31 14:45:14 +0300

    refactor: remove unused click handlers and state variables

--- src/components/ui/layout-grid.tsx
+++ src/components/ui/layout-grid.tsx
@@ -12,48 +12,25 @@ type Card = {
 };
 
 export const LayoutGrid = ({ cards }: { cards: Card[] }) => {
-  const [selected, setSelected] = useState<Card | null>(null);
-  const [lastSelected, setLastSelected] = useState<Card | null>(null);
-
-  const handleClick = (card: Card) => {
-    setLastSelected(selected);
-    setSelected(card);
-  };
-
-  const handleOutsideClick = () => {
-    setLastSelected(selected);
-    setSelected(null);
-  };
-
   return (
     <div className="w-full h-full p-10 grid grid-cols-1 md:grid-cols-3  max-w-7xl mx-auto gap-4 ">
       {cards.map((card, i) => (
-        <div key={i} className={cn(card.className, '')}>
+        <div key={i} className={cn(card.className, 'relative')}>
           <motion.div
-            onClick={() => handleClick(card)}
             className={cn(
               card.className,
-              'relative overflow-hidden',
-              selected?.id === card.id
-                ? 'rounded-lg cursor-pointer absolute inset-0 h-1/2 w-full md:w-1/2 m-auto z-50 flex justify-center items-center flex-wrap flex-col'
-                : lastSelected?.id === card.id
-                ? 'z-40 bg-white rounded-xl h-full w-full'
-                : 'bg-white rounded-xl h-full w-full'
+              ' overflow-hidden rounded-lg inset-0 h-1/2 w-full md:w-1/2 m-auto z-50 flex justify-center items-center flex-wrap flex-col'
             )}
             layout
           >
-            {selected?.id === card.id && <SelectedCard selected={selected} />}
             <BlurImage card={card} />
           </motion.div>
         </div>
       ))}
       <motion.div
-        onClick={handleOutsideClick}
         className={cn(
-          'absolute h-full w-full left-0 top-0 bg-black opacity-0 z-10',
-          selected?.id ? 'pointer-events-auto' : 'pointer-events-none'
+          'absolute h-full w-full left-0 top-0 bg-black opacity-0 z-10'
         )}
-        animate={{ opacity: selected?.id ? 0.3 : 0 }}
       />
     </div>
   );
