fe359ada0ceb67278338580813dfc94fa056ef4e
aesweb <aes.sncr@gmail.com>
2024-02-06 00:07:17 +0300

    fix: update instagram link to correct location

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -33,7 +33,7 @@ const Contact = () => {
             <div className="text-xl pt-2">instagram</div>
           </Link>
           <Link
-            href="https://www.instagram.com/kaptanmedya/"
+            href="https://www.google.com.tr/maps/place/Ba%C4%9Flarba%C5%9F%C4%B1,+%C4%B0stasyon+Cd.+No:136,+41700+Dar%C4%B1ca%2FKocaeli/@40.7582394,29.3840402,17z/data=!3m1!4b1!4m6!3m5!1s0x14cae06efa4919fb:0x26ea8abe2f08d819!8m2!3d40.7582394!4d29.3866151!16s%2Fg%2F11fl9grh0k?entry=ttu"
             target="blank"
             className="items-center justify-center flex flex-col hover:scale-105 transition-all duration-300 ease-in-out"
           >
