d9193651248032184d541df7e974473ea834d390
aesweb <aes.sncr@gmail.com>
2024-03-12 00:16:51 +0300

    Remove unused imports and update about section

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Image from 'next/image';
 
 const About = () => {
@@ -16,22 +15,45 @@ const About = () => {
           'Siz hayal edin Kaptan Medya Organizasyon gerçekleştirsin.'
         </div>
         <div className="text-md max-w-2xl font-thin">
-          Zarif, şık, çekici, eğlenceli ve çarpıcı bir gerçeklik ile rüya gibi
-          bir kutlama planlayarak zamanın çok ötesinde her detayı ile sofistike
-          bir estetikle planlıyoruz.
+          Düğün Organizasyonunuz için sunduğumuz birbirinden güzel hizmetler
+          eşliğinde, unutulmayacak kadar güzel bir masalın prensi ve prensesi
+          olmaya hazır mısınız? Kaptan Medya Organizasyon olarak, isteklerinize
+          önem veren yetenekli bir ekibe sahibiz. Bu ekibimizle düğününüzde sizi
+          mutlu etmek, eğlendirmek ve tüm dostlarınızla keyifli anlar yaşamanızı
+          sağlamak için titiz bir çalışma yürütüyoruz.
           <br />
           <br />
+          Peki bizimle iletişime geçtiğiniz andan itibaren sürecimiz nasıl
+          ilerliyor? <br />
           <br />
-          Hayallerinizdeki kutlamayı gerçeğe dönüştürürken, her detayı özenle
-          düşünüyor ve planlıyoruz. Kaptan Medya, zarafetin, şıklığın ve
-          eğlencenin mükemmel bir birleşimini sunmak için size hizmet sunuyor.
+          Öncelikle ilk olarak size kısa bir bilgilendirme yapıyoruz. Daha sonra
+          da hem sizi daha yakından tanımak hem de hizmet detaylarını anlatmak
+          için bir ön görüşme tarihi belirliyoruz. Görüşmemiz sırasında düğün
+          konseptinizi belirlemekle işe koyuluyor, daha sonra ihtiyaçlarınızı
+          karşılayabileceğiniz paketlerimiz hakkında bilgi veriyoruz. Bu
+          paketlerimizden bütçenize göre dilediğinizi seçebiliyorsunuz. <br />
+          <br />
+          Konseptlerimizde şu hizmetler yer alıyor; şık bir masa konsepti,
+          Supla, peçete, şamdan, canlı çiçek aranjmanı, yürüyüş yolları, ışık
+          sistemleri, dj, orkestra özel görsel model imkanlarına Kaptan Medya
+          aracılığıyla sahip olabiliyorsunuz. Düğün organizasyonu konusunda
+          tecrübeli olan ekibimizin, baştan sona akış planını da yaptıklarını
+          söyleyelim.
+          <br />
+          <br /> Organizasyonlara getirdiğimiz yeni bakış açımız ve
+          konseptlerimizle o değerli geceyi eşsiz ve unutulmayacak hâle
+          getireceğimizden eminiz. Mekân süslemesinden ikramlara, dans
+          gösterilerinden giriş müziğine geniş hizmet yelpazemizden yararlanarak
+          stressiz bir süreç geçirebilirsiniz. Kısacası siz ne hayal
+          ettiklerinizi bize söyleyin ve hiçbir ayrıntıyı düşünmeden bu değerli
+          günün keyfini çıkarın.
           <br />
           <br />
-          <br />
-          Kaptan Medya, hayatınızdaki her anın kıymetini biliyor ve bu anları
-          daha da özel kılmak için burada. Mutluluğunuzu paylaşma ayrıcalığını
-          bizimle yaşayın. Kaptan Medya olarak sizinle geçirdiğimiz her an, özel
-          ve unutulmaz olacak.
+          Eğer Kaptan Medya Darıca firmamız hakkında daha fazla bilgi almak ve
+          fiyatlarımızı öğrenmek isterseniz, sayfanın sağ alt kısmında bulunan
+          whatsapp butonuna tıklayarak bize ulaşabilirsiniz. Whatsapp dışında
+          sayfadaki telefon numarası üzerinden de bizimle iletişime
+          geçebileceğinizin bilgisini verelim.
         </div>
       </div>
     </section>
