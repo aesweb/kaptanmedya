05a67aa9eeadea4751bc913b5ddbbb665c9c0c8c
aesweb <aes.sncr@gmail.com>
2024-03-26 03:39:33 +0300

    fix: update title and language meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ import { SpeedInsights } from '@vercel/speed-insights/next';
 const inter = Figtree({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Kaptan Medya',
+  title: 'Kaptan Medya Organizasyon',
   description:
     'Kaptan Medya Organizasyon | Düğün Davet ve Organizasyon | Kocaeli | İstanbul',
   icons: ['/favicon.ico?=v4'],
@@ -22,7 +22,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="tr">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="keywords"
+          content="Kocaeli, Gebze, Darıca, İzmit, İstanbul, organizasyon firmalarından biri olan Kaptan Medya organizasyon kurumsal etkinlikler, event, Şirket piknikleri, lansman, festivaller, gala, açılış, organizasyonları, çadır, minder, armut minder kiralama"
+        />
+        <meta
+          name="description"
+          content="Gala, Açılış, Tanıtım, Lansman, Yılbaşı, Event, Minder kiralama, festival, İstanbul, Kocaeli, Gebze, Darıca organizasyon firmaları, event, Kaptan Medya organizasyon, profesyonel çadır sistemleri, çadır kiralama, kurumsal etkinlikler, kokteyl"
+        />
+        <meta
+          name="keywords"
+          content="Gala, Lansman, festival, Yılbaşı, Kurumsal etkinlikler, İstanbul, Kocaeli, Gebze, Darıca, minder kiralama, çadır kiralama, Karaoke partileri, Kaptan Medya Organizasyon tecrübesiyle size özel çözümler sunar"
+        />
+        <meta name="author" content="Kaptan Medya Organizasyon" />
+        <meta name="robots" content="index, follow" />
+        <meta name="googlebot" content="index, follow" />
+      </head>
       <body className={inter.className}>
         <Suspense fallback="...">
           <Navbar />
